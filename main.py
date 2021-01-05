f5e860df2eb0afbb73d47c1b96714c5e5cba96e3
Darin Korabov <korabovdarin@gmail.com>
2021-01-05 22:51:22 +0200

    description added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+"""
+    git status
+    git branch
+    git checkout <branch_name>
+    git add <file/s>
+    git add .           - add all files
+    git commit -m "message"
+    git merge <branch_name>
+
+"""
+
 import pandas as pd
+
